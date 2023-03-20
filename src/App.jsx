ec88da31a0b01056f32d55042cda09529f5972e9
Miguelcodecrypto <miguels.codecrypto@gmail.com>
2023-03-20 19:08:32 +0100

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,23 @@ import './App.css'
 import { useForm } from 'react-hooks-forms'
 
 function App() {    
-  const{register, handleSubmit} = useForm();
-
-    function onSubmit(data) {
-      console.log(data)
-    }
-
-    return (
+  
+  const {register, handleSubmit} = useForm();
+  
+  function onSubmit(data) { 
+  console.log(data)
+  
+  }   
+  
+  return (
      <div className="App">
-    
        <form onSubmit={handleSubmit (onSubmit)}>
          <input {...register("campo1")} /><br></br>
          <input {...register("campo2")} /><br></br>
          <input type="submit" />
        </form> 
     </div>
+  
   )
 }
 
